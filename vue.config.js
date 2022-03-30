d9b18618fb33a8f91d74c33a2da6e51d16470892
Oscar <oscarskapee@gmail.com>
2022-03-29 21:31:41 -0600

    Cambios

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/index.html'
+    },
+    // when using the entry-only string format,
+    // template is inferred to be `public/subpage.html`
+    // and falls back to `public/index.html` if not found.
+    // Output filename is inferred to be `subpage.html`.
+    //subpage: 'src/subpage/main.js'
+  }
 })
